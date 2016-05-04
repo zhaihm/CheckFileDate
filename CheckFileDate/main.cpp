03cf0ad27b980ae2527f146f22abdead9a0e7a3f
Martin Zhai <hmpro74@hotmail.com>
2016-05-04 21:40:40 +0800

    finish tool

---
+++ CheckFileDate/main.cpp
@@ -0,0 +1,76 @@
+#include "getopt.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <Windows.h>
+
+int find_all_files(const char *path, const char *date);
+
+int main(int argc, char *argv[])
+{
+    int opt;
+    char *date = NULL;
+
+    while ((opt = getopt(argc, argv, "n:")) != -1) {
+        switch (opt) {
+        case 'n':
+            date = optarg;
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-n date]\n",
+                argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Expected argument after options\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (date != NULL) {
+        find_all_files(argv[optind], date);
+    }
+}
+
+int find_all_files(const char *path, const char *date)
+{
+    std::string strFind = path;
+    strFind += "\\*.*";
+
+    WIN32_FIND_DATA findData;
+    HANDLE hFind = ::FindFirstFile(strFind.c_str(), &findData);
+    if (INVALID_HANDLE_VALUE == hFind)
+        return -1;
+
+    do {
+        if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            if (strcmp(findData.cFileName, ".") != 0 && strcmp(findData.cFileName, "..") != 0) {
+                strFind = path;
+                strFind += "\\";
+                strFind += findData.cFileName;
+                find_all_files(strFind.c_str(), date);
+            }
+        }
+        else {
+            SYSTEMTIME st;
+            sscanf(date, "%d-%d-%d %d:%d:%d", &st.wYear, &st.wMonth, &st.wDay, &st.wHour, &st.wMinute, &st.wSecond);
+
+            FILETIME ft, ftFileLastWrite;
+            SystemTimeToFileTime(&st, &ft);
+            FileTimeToLocalFileTime(&findData.ftLastWriteTime, &ftFileLastWrite); // findData.ftLastWriteTime is UTC time
+            
+            if (CompareFileTime(&ftFileLastWrite, &ft) == 1) {
+                printf("%s\\%s\n", path, findData.cFileName);
+
+                std::string fullPath = path;
+                fullPath += "\\";
+                fullPath += findData.cFileName;
+                //printf("fullPath=%s\n", fullPath.c_str());
+            }
+        }
+    } while (::FindNextFile(hFind, &findData));
+
+    ::FindClose(hFind);
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CheckFileDate
 A tool to check file modify date.
+
+### Introduction
+We want to know when did folder's files modified sometime, but on Windows, only add/delete file/sub-folder change folder's modify time. So I write this tool to easily **list recently modified files**.
+
+### Usage
+`CheckFileDate [-n date] path`
+
+* **-n** list files newer than *date*, *date* **must** be in format "2016-05-04 21:30:00"
